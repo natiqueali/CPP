5283cfb0e8f2b4396225ebb9fa32490e6da1dfdf
natiqueali <muhammadnatiqueali@gmail.com>
2024-10-26 20:41:56 +0000

    updates

---
+++ ass1.cpp
@@ -0,0 +1,14 @@
+// Your First C++ Program
+
+#include <iostream>
+
+int main() {
+    int sum;
+    int x;
+    x = 1;
+    sum = 0;
+    sum = sum + x;
+
+    std::cout << "The value of variable sum is = ",sum;
+    return 0;
+}
